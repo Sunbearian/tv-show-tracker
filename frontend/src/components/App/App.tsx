fbb333951fe1526344f77d513f6621603849ecfc
Sunbearian <stigcollins@icloud.com>
2022-12-20 21:43:15 +0000

    Started the NavBar component

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import NavBar from "../NavBar/NavBar";
 
 function App() {
 	return (
 		<div className="App">
+			<NavBar></NavBar>
 			<header className="App-header">
 				<h1>TV Shower Tracker</h1>
 			</header>
