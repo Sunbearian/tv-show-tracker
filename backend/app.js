919651f63bbca07f1ddfb11b4409dc520216e4c6
Sunbearian <stigcollins@icloud.com>
2023-01-01 22:42:52 +0000

    Set up database link, routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import usersRouter from "./routes/users.js";
+import tvShowsRouter from "./routes/tvshows.js";
 
 const PORT = process.env.PORT ?? 3500;
 
@@ -9,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
+
+app.use("api/users", usersRouter);
+app.use("api/tvshows", tvShowsRouter);
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`);
