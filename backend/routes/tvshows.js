50303747257a3d3d0c9cb47b359848e1c0f9ebac
Steven Collins <91161452+Sunbearian@users.noreply.github.com>
2023-01-07 21:11:30 +0000

    added delete functionaility (#9)

--- backend/routes/tvshows.js
+++ backend/routes/tvshows.js
@@ -1,7 +1,7 @@
 import express, { Router } from "express";
 const tvShowsRouter = express.Router();
 
-import { getUserTVShows, addTVShow } from "../models/tvshows.js";
+import { getUserTVShows, addTVShow, deleteShow } from "../models/tvshows.js";
 
 tvShowsRouter.get("/:id", async (req, res) => {
 	const id = req.params.id;
@@ -13,6 +13,13 @@ tvShowsRouter.get("/:id", async (req, res) => {
 tvShowsRouter.post("/", async (req, res) => {
 	const tvShow = req.body;
 	const newTVShow = await addTVShow(tvShow);
+
+	res.status(201).json({ success: true, payload: newTVShow });
+});
+
+tvShowsRouter.delete("/:id", async (req, res) => {
+	const showId = req.params.id;
+	const newTVShow = await deleteShow(showId);
 
 	res.status(201).json({ success: true, payload: newTVShow });
 });
