f7faf04a6e08f0e04753d453261c40f49e812ad9
Sunbearian <stigcollins@icloud.com>
2023-05-09 16:34:35 +0100

    Removed landing page css after adding tw css

--- frontend/src/routes/LandingPage.tsx
+++ frontend/src/routes/LandingPage.tsx
@@ -1,18 +1,21 @@
 import LoginButton from "../components/NavBar/LoginButton";
 import SignupButton from "../components/NavBar/SignupButton";
-import "./LandingPage.css";
 
 export default function Main() {
 	return (
-		<header className="App-header">
-			<h1 className='text-4xl font-bold'>TV Show Tracker</h1>
-			<p>Can't remember what series you last watched of your favourite show?</p>
-			<p>Want to track the series you're watching?</p>
-			<p>
-				TV Show Tracker is a simple app to help you log what you've watched and
-				keep track of where you are
-			</p>
-			<div className="landing-page-button">
+		<header className="App-header text-white flex flex-col items-center">
+			<h1 className="text-6xl font-bold m-12">TV Show Tracker</h1>
+			<div className="flex flex-col items-center mb-6 text-lg">
+				<p className="m-1">
+					Can't remember what series you last watched of your favourite show?
+				</p>
+				<p className="m-1">Want to track the series you're watching?</p>
+				<p className="m-1">
+					TV Show Tracker is a simple app to help you log what you've watched
+					and keep track of where you are.
+				</p>
+			</div>
+			<div className="space-x-10">
 				<LoginButton />
 				<SignupButton />
 			</div>
