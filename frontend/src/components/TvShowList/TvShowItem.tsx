0e83749ca71f91921deddc7a570fb481eac301cc
Sunbearian <stigcollins@icloud.com>
2023-05-09 16:55:11 +0100

    Updated css on TvShowItem

--- frontend/src/components/TvShowList/TvShowItem.tsx
+++ frontend/src/components/TvShowList/TvShowItem.tsx
@@ -23,14 +23,17 @@ export default function TvShowItem({ tvShow, deleteShow }: props) {
 	}
 
 	return (
-		<div className="rounded border border-solid border-white flex flex-col space-y-1 px-10 py-6 items-start max-w-xl m-4">
-			<h3>{show_name}</h3>
+		<div className="rounded border border-solid border-white flex flex-col space-y-1 px-10 py-6 items-start max-w-xl m-4 text-white">
+			<h3 className="text-2xl font-bold ">{show_name}</h3>
 			<p>
 				Series: {series_watched} / {total_series}
 			</p>
 			<p>Last Watched: {convertedDate}</p>
 			<p>{rating} / 5</p>
-			<button onClick={onClick} className="self-end">
+			<button
+				onClick={onClick}
+				className="self-end bg-white p-2 rounded-full hover:bg-red-700"
+			>
 				🗑️
 			</button>
 		</div>
