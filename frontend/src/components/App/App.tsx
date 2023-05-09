f8cb78bb1a98d7ced8f78b27cab35d693f393d91
Sunbearian <stigcollins@icloud.com>
2023-05-09 16:43:57 +0100

    Added tw css to App.tsx

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -8,7 +8,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 function App() {
 	const { isAuthenticated } = useAuth0();
 	return (
-		<div className="App">
+		<div className="flex flex-col items-center">
 			<NavBar></NavBar>
 
 			<Routes>
